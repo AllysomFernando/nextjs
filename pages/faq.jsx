8fe3570d52d6f459f038af9a320c10f34a9353df
Allysom Renczenczen <82781674+AllysomFernando@users.noreply.github.com>
2023-08-18 22:12:44 -0300

    Adicionando Páginas

---
+++ pages/faq.jsx
@@ -0,0 +1,7 @@
+export default function FAQpage (){
+    return(
+        <div>
+            <h1>FAQ Developed by Allysom</h1>
+        </div>
+    )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+	return (
+		<div>
+			<a href="/faq">Ir para faq</a>
+		</div>
+	);
+}
