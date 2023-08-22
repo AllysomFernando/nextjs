c5db58978d648317ff48291ed8b0dd04f10244ef
AllysomFernando <allysom.renczenczen18@gmail.com>
2023-08-22 15:45:00 -0300

    adding global styled components on index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,35 @@
-import Link from "../src/components/Link"
+import React from "react";
+import Link from "../src/components/Link";
+
+function GlobalStyle(){
+	return(
+		<style jsx>{`
+			body{
+				font-family: sans-serif;
+			}
+		`}</style>
+	)
+}
+function Title({ children, as }) {
+  const Tag = as;
+  return (
+    <React.Fragment>
+      <Tag>{children}</Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: red;
+		  font-family: sans-serif;
+        }
+      `}</style>
+    </React.Fragment>
+  );
+}
+
 export default function HomePage() {
-	return (
-		<div>
-			<h1>Home Developed by Allysom</h1>
-			<Link href="/faq">Ir para a página do FAQ</Link>
-		</div>
-	);
+  return (
+    <div>
+      <Title as="h2">Home - Developed by Allysom</Title>
+      <Link href="/faq">Ir para a página do FAQ</Link>
+    </div>
+  );
 }
