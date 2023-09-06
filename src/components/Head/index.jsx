60a3128b208ec5bdf3654c1c5f0bfc1bc63e888e
AllysomFernando <allysom.renczenczen18@gmail.com>
2023-09-06 19:30:47 -0300

    Tirando o title fixo do head e deixando ele como children

--- src/components/Head/index.jsx
+++ src/components/Head/index.jsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 export default function Heads({children}){
     return(
         <Head>
-            <title>Find By</title>
             <title>{children}</title>
         </Head>
     )
